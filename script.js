14435f4c17309a50af1f9a15cfef6d903e4eaab0
ftarakdjian <facu.tarakdjian@gmail.com>
2023-06-09 14:41:30 -0300

    documento js

---
+++ script.js
@@ -0,0 +1,32 @@
+  // Pregunta 1
+var respuesta1 = prompt("1. ¿Cuántos Grand Slam ha ganado Federer?");
+if (respuesta1 === "20") {
+    console.log("¡Correcto!");
+} else {
+    console.log("Incorrecto. La respuesta correcta es 20.");
+}
+
+// Pregunta 2
+var respuesta2 = prompt("2. ¿Cuál es el torneo que Federer ha ganado más veces?");
+if (respuesta2.toLowerCase() === "wimbledon") {
+    console.log("¡Correcto!");
+} else {
+    console.log("Incorrecto. La respuesta correcta es Wimbledon.");
+}
+
+// Pregunta 3
+var respuesta3 = prompt("3. ¿Cuál es el torneo que más le ha costado ganar a Federer?");
+if (respuesta3.toLowerCase() === "roland garros") {
+    console.log("¡Correcto!");
+} else {
+    console.log("Incorrecto. La respuesta correcta es Roland Garros.");
+}
+
+// Pregunta 4
+var respuesta4 = prompt("4. ¿Quién ha sido el rival más difícil de Federer?");
+if (respuesta4.toLowerCase() === "rafael nadal") {
+    console.log("¡Correcto!");
+} else {
+    console.log("Incorrecto. La respuesta correcta es Rafael Nadal.");
+}
+
